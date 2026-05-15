a28041936dace0ccf6fa7665eda1a0045e10f02c
JPTFL <js.pp@gmx.de>
2026-05-15 15:14:28 +0200

    Initial project creation, adds task 1, adds standard gitignore

---
+++ omnet_tictoc/src/Tx1.cc
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class Txc1: public cSimpleModule {
+public:
+    int forwardCount = 0;
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(Txc1);
+
+void Txc1::initialize() {
+    if (strcmp("tic", getName()) == 0) {
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
+
+void Txc1::handleMessage(cMessage *msg) {
+    ++forwardCount;
+    EV << "forwardCount: " << forwardCount << endl;
+
+    if (forwardCount >= 20){
+        delete msg;
+        return;
+    } else {
+        send(msg, "out");
+    }
+}
